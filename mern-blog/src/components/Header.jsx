012dde9a2c12f44df305f7d485294cfda059878c
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-22 18:15:32 +0100

    tailwind is working started the production of the navbar

--- mern-blog/src/components/Header.jsx
+++ mern-blog/src/components/Header.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
 import {images} from '../constants';
 
+
+const navItemsInfo = [
+  {name: "Home"},
+  {name: "Articles"},
+  {name: "Pages"},
+  {name: "Pricing"},
+  {name: "FAQ"}
+]
+
+const NavItem = ({name}) => {
+  return (
+    <li className="relative group">
+      <a href="/" className="px-4 py-2 group-hover:text-orange-800">
+        {name}
+      </a>
+      <span className="text-orange-800 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+        /
+      </span>
+    </li>
+  );
+};
+
 const Header = () => {
   return (
     <div>
       <section>
-        <header className="container mx-auto px-5 flex justify-between">
+        <header className="container mx-auto px-5 flex justify-between py-4 items-center">
           <div>
             <img src={images.logo} alt="logo" width="115 px" />
           </div>
-          <div className="flex gap-x-8">
-            <ul className="flex gap-x-8">
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/">Articles</a>
-              </li>
-              <li>
-                <a href="/">Pages</a>
-              </li>
-              <li>
-                <a href="/">Pricing</a>
-              </li>
-              <li>
-                <a href="/">FAQ</a>
-              </li>
-
-                <button>Sign In</button>
+          <div className="gap-x-9">
+            <ul className="flex gap-x-5 font-semibold">
+              {navItemsInfo.map((item) => (
+                <NavItem key={item.name} name={item.name}/>
+              ))}
+              <button className="border-2 border-orange-800 px-6 py-2 rounded-full text-orange-800 font-semibold hover:bg-orange-300 hover:text-white hover:border-orange-300 transition-all duration-200">
+                Sign In
+              </button>
             </ul>
           </div>
         </header>
